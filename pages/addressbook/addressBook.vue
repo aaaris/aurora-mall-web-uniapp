eaea43390c01c7d7b71f1ef82659e3a4bbbaeddd
aris <2997600742@qq.com>
2022-11-18 13:35:09 +0800

    feat(addressBookPage): 增加按钮用于新建收货地址

--- pages/addressbook/addressBook.vue
+++ pages/addressbook/addressBook.vue
@@ -1,22 +1,27 @@
 <template>
 	<view>
 		<u-navbar :border-bottom="false" title="我的收货地址"></u-navbar>
-		<view>
+		<view class="u-flex-col u-row-around">
 			<!-- 收货地址项 -->
-			<u-card padding="20" :show-head="false" :show-foot="false" border-radius="20"
-				v-for="(item,index) in addressItemList" :key="index">
-				<view slot="body" class="address-item-bar">
-					<view class="address-item">
-						<text>{{item.name}}</text>&nbsp;
-						<text>{{item.phone}}</text>&nbsp;
-						<u-tag text="默认" shape="circle" type="error" v-if="item.inUse" />
-						<text style="display: block; font-size: 12px; margin-top: 15rpx;">
-							{{item.desc}}
-						</text>
+			<view>
+				<u-card padding="20" :show-head="false" :show-foot="false" border-radius="20"
+					v-for="(item,index) in addressItemList" :key="index">
+					<view slot="body" class="address-item-bar">
+						<view class="address-item">
+							<text>{{item.name}}</text>&nbsp;
+							<text>{{item.phone}}</text>&nbsp;
+							<u-tag text="默认" shape="circle" type="error" v-if="item.inUse" />
+							<text style="display: block; font-size: 12px; margin-top: 15rpx;">
+								{{item.desc}}
+							</text>
+						</view>
+						<u-icon name="edit-pen" size="40" @click="gotoEdit(item)"></u-icon>
 					</view>
-					<u-icon name="edit-pen" size="40" @click="gotoEdit(item)"></u-icon>
-				</view>
-			</u-card>
+				</u-card>
+			</view>
+			<view class="u-m-30">
+				<u-button type="error" @click="gotoEdit()" shape="circle">新增收货地址</u-button>
+			</view>
 		</view>
 	</view>
 </template>
@@ -33,20 +38,20 @@
 						name: "张三",
 						phone: "13511111111",
 						desc: "翻斗市区翻斗大街A1104",
-						region:"",
+						region: "",
 						inUse: true,
 					}, {
 						name: "李四",
 						phone: "13511111111",
 						desc: "翻斗市区翻斗大街A1104",
-						region:"",
+						region: "",
 						inUse: false,
 					},
 					{
 						name: "王五",
 						phone: "13511111111",
 						desc: "翻斗市区翻斗大街A1104",
-						region:"",
+						region: "",
 						inUse: false,
 					}
 				]
@@ -59,9 +64,15 @@
 		methods: {
 			gotoEdit(target) {
 				console.log("go to edit address...")
-				uni.navigateTo({
-					url: "/pages/addressbook/addressBookEdit?item=" + encodeURIComponent(JSON.stringify(target))
-				})
+				if (target)
+					uni.navigateTo({
+						url: "/pages/addressbook/addressBookEdit?item=" + encodeURIComponent(JSON.stringify(target))
+					})
+				else {
+					uni.navigateTo({
+						url: "/pages/addressbook/addressBookEdit"
+					})
+				}
 			}
 		}
 	}
@@ -69,6 +80,10 @@
 <style lang="scss">
 	page {
 		background-color: #f7f7f7;
+	}
+
+	.wrap {
+		min-height: 100vh;
 	}
 
 	// 地址项框
