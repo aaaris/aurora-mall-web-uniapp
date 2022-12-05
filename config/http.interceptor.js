8927d7280806ec7799c50a0240b9e01ed9c2c2c1
aris <2997600742@qq.com>
2022-12-05 12:05:13 +0800

    feat: 使用uView JS库 配置http拦截和api统一管理

--- config/http.interceptor.js
+++ config/http.interceptor.js
@@ -6,14 +6,15 @@
 const install = (Vue, vm) => {
 	// 此为自定义配置参数，具体参数见上方说明
 	Vue.config.globalProperties.$u.http.setConfig({
-		baseUrl: '', // 请求的本域名
+		baseUrl: 'http://172.30.206.89:8201', // 请求的本域名
+		// baseUrl: 'https://mock.apifox.cn/m1/1426632-0-default',
 		method: 'POST',
 		// 设置为json，返回后会对数据进行一次JSON.parse()
 		dataType: 'json',
 		showLoading: true, // 是否显示请求中的loading
 		loadingText: '请求中...', // 请求loading中的文字提示
 		loadingTime: 800, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
-		originalData: false, // 是否在拦截器中返回服务端的原始数据
+		originalData: true, // 是否在拦截器中返回服务端的原始数据
 		loadingMask: true, // 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
 		// 配置请求头信息
 		header: {
@@ -22,7 +23,7 @@ const install = (Vue, vm) => {
 	});
 
 	// 请求拦截部分，如配置，每次请求前都会执行
-	Vue.prototype.$u.http.interceptor.request = (config) => {
+	Vue.config.globalProperties.$u.http.interceptor.request = (config) => {
 		// 引用token
 		// 方式一，存放在vuex的token，假设使用了uView封装的vuex方式
 		// 见：https://uviewui.com/components/globalVariable.html
@@ -49,25 +50,26 @@ const install = (Vue, vm) => {
 	}
 	
 	// 响应拦截，如配置，每次请求结束都会执行本方法
-	Vue.prototype.$u.http.interceptor.response = (res) => {
+	Vue.config.globalProperties.$u.http.interceptor.response = (res) => {
 		if (res.code == 200) {
 			// res为服务端返回值，可能有code，result等字段
 			// 这里对res.result进行返回，将会在this.$u.post(url).then(res => {})的then回调中的res的到
 			// 如果配置了originalData为true，请留意这里的返回值
-			return res.result;
+			return res.data;
 		} else if (res.code == 201) {
-			// 假设201为token失效，这里跳转登录
+			// 尝试更新token
+			// 假设201为token失效，这里跳转登录 
 			vm.$u.toast('验证失败，请重新登录');
 			setTimeout(() => {
 				// 此为uView的方法，详见路由相关文档
 				vm.$u.route('/pages/user/login')
 			}, 1500)
 			return false;
-		} else {
+		} else { 
 			// 如果返回false，则会调用Promise的reject回调，
 			// 并将进入this.$u.post(url).then().catch(res=>{})的catch回调中，res为服务端的返回值
 			return false;
-		}
+		} 
 	}
 }
 
--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 import App from './App'
 import uView from './uni_modules/vk-uview-ui';
+// http拦截器，此为需要加入的内容，如果不是写在common目录，请自行修改引入路径
+import httpInterceptor from './config/http.interceptor.js'
+// http接口API集中管理引入部分
+import httpApi from '@/api/http.api.js'
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+	...App
 })
-// http拦截器，此为需要加入的内容，如果不是写在common目录，请自行修改引入路径
-import httpInterceptor from '@/common/http.interceptor.js'
-// 这里需要写在最后，是为了等Vue创建对象完成，引入"app"对象(也即页面的"this"实例)
-Vue.use(httpInterceptor, app)
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-    // 使用 uView UI
-    app.use(uView)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	// 使用 uView UI
+	app.use(uView)
+	// 使用 http 拦截器
+	app.use(httpInterceptor)
+	// 使用 api 统一管理
+	app.use(httpApi)
+	return {
+		app
+	}
 }
 // #endif
