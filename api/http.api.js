43c22cd0927401a1fe860fcf92ae6d661281d54f
aris <2997600742@qq.com>
2022-12-14 15:20:38 +0800

    feat: 增加api接口

--- api/http.api.js
+++ api/http.api.js
@@ -7,11 +7,23 @@ const install = (Vue) => {
 	// 将各个定义的接口名称，统一放进对象挂载到vm.$u.api(因为vm就是this，也即this.$u.api)下
 	vm.$u.api = {
 		// wx用户登录接口
-		login: (params = {}) => vm.$u.get('/user/login', params),
+		login: (params = {}) => vm.$u.post('/user/login', params),
 		// 购物车获取购物项
 		cart: {
-			getCartItems: (params = {}) => vm.$u.get('/cart/cartItem', params)
-
+			getCartItems: (params = {}) => vm.$u.get('/cart/cartItem', params),
+			getCount: (params = {}) => vm.$u.get('/cart/count', parmas),
+			addItem: (params = {}) => vm.$u.post('/cart/add', parmas),
+			deleteItem: (params = {}) => vm.$u.post('/cart/delete', parmas),
+			updateItemCount: (params = {}) => vm.$u.post('/cart/updateItemCount', params)
+		},
+		prod: {
+			search: (params = {}) => vm.$u.get('/product/getProdList', params),
+			getDetail: (params = {}) => vm.$u.get('/product/details', params),
+		},
+		order: {
+			getOrderList: (params = {}) => vm.$u.get('/order/getOrderList', params),
+			updateStat: (params = {}) => vm.$u.post('/order/updateStat', params),
+			createOrder: (params = {}) => vm.$u.post('/order/add', params),
 		}
 	};
 }
